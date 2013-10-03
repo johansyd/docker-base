d50c1485b3ce61312027ae2bcdbc71b8ac5bcf0e
Johan Sydseter <johan@aptoma.com>
2013-10-03 19:37:44 +0200

    adding start script to start the docker containers

--- start.sh
+++ start.sh
@@ -2,5 +2,41 @@
 # start base image
 # sudo docker run -i -p 5300:80 -v ~/src:/home/docker/src -t $image /bin/bash;
 
-sudo docker run $@;
+    image=  port=  src=  
+    while getopts i:p:s: opt; do
+        case $opt in
+        i)
+            image=$OPTARG
+            ;;
+        p)
+            port=$OPTARG
+            ;;
+        s)
+            src=$OPTARG
+            ;;
+        *)
+                break
+                ;;
+        esac 
+    done;
+    shift $((OPTIND - 1));
 
+if [ -z $image ] || [ -z $port ] || [ -z $src ]; then
+    echo "";
+    echo "Usage: start.sh -i <hostname and image name> -p <port to webserver> -s <directory to mount>"
+    exit 0;
+fi
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+image_dir="$dir/.$image";
+
+if [ ! -d "$image_dir" ]; then
+    $dir/build.sh --image $image;
+fi
+
+if [ ! -d "$src" ]; then
+    mkdir $src;
+fi
+
+sudo docker run -i -p $port:80 -v $src:/home/docker/src -h $image -t $image /bin/bash;
+exit 1;
